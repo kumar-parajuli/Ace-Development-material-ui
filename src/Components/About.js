514e3eb799b87cdd6805e291a1c66fd347516fee
parajuli rahul <parajulirahul98@gmail.com>
2023-06-21 01:50:26 +0545

    code...for aboutpage

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,151 @@
-import React from 'react'
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/styles";
+import { makeStyles } from "@mui/styles";
+import history from "../assets/history.svg";
 
-const About = () => {
+const useStyles = makeStyles((theme) => ({
+  missionStatement: {
+    fontStyle: "italic",
+    fontWeight: 300,
+    fontSize: "1.5rem",
+    maxWidth: "50em",
+    lineHeight: 1.4,
+  },
+  rowContainer: {
+    paddingLeft: "5em",
+    paddingRight: "5em",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "1.5em",
+      paddingRight: "1.5em",
+    },
+  },
+  avatar: {
+    height: "25em",
+    width: "25em",
+    [theme.breakpoints.down("sm")]: {
+      height: "20em",
+      width: "20em",
+      maxHeight: 300,
+      maxWidth: 300,
+    },
+  },
+}));
+
+const About = (props) => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
-    <div>About</div>
-  )
-}
+    <Grid container direction="column">
+      <Grid item className={classes.rowContainer} sx={{ marginTop: "2em" }}>
+        <Typography variant="h2">About us</Typography>
+      </Grid>
+      <Grid
+        item
+        container
+        justifyContent="center"
+        className={classes.rowContainer}
+      >
+        <Typography variant="h4" className={classes.missionStatement}>
+          Whether it be person to person, business to consumer, or an individual
+          to their interests, technology is meant to bring us closer to what we
+          care about in the best way possible. Arc Development will use that
+          principle to provide fast, modern, inexpensive, and aesthetic software
+          to the Midwest and beyond.
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        container
+        className={classes.rowContainer}
+        style={{ marginTop: "10em", marginBottom: "10em" }}
+        direction={matchesMD ? "column" : "row"}
+        alignItems={matchesMD ? "center" : undefined}
+        justify="space-between"
+      >
+        <Grid item>
+          <Grid
+            item
+            container
+            direction="column"
+            lg
+            style={{ maxWidth: "35em" }}
+          >
+            <Grid item>
+              <Typography
+                align={matchesMD ? "center" : undefined}
+                variant="h4"
+                gutterBottom
+              >
+                History
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                variant="body1"
+                align={matchesMD ? "center" : undefined}
+                paragraph
+                style={{ fontWeight: 700, fontStyle: "italic" }}
+              >
+                We're the new kid on the block
+              </Typography>
+              <Typography
+                variant="body1"
+                align={matchesMD ? "center" : undefined}
+                paragraph
+              >
+                Founded in 2019, we’re ready to get our hands on the world’s
+                business problems.
+              </Typography>
+              <Typography
+                variant="body1"
+                align={matchesMD ? "center" : undefined}
+                paragraph
+              >
+                It all started with one question: Why aren’t all businesses
+                using available technology? There are many different answers to
+                that question: economic barriers, social barriers, educational
+                barriers, and sometimes institutional barriers.
+              </Typography>
+              <Typography
+                variant="body1"
+                align={matchesMD ? "center" : undefined}
+                paragraph
+              >
+                We aim to be a powerful force in overcoming these obstacles.
+                Recent developments in software engineering and computing power,
+                compounded by the proliferation of smart phones, has opened up
+                infinite worlds of possibility. Things that have always been
+                done by hand can now be done digitally and automatically, and
+                completely new methods of interaction are created daily. Taking
+                full advantage of these advancements is the name of the game.
+              </Typography>
+              <Typography
+                variant="body1"
+                align={matchesMD ? "center" : undefined}
+                paragraph
+              >
+                All this change can be a lot to keep up with, and that’s where
+                we come in.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid item container justify="center" lg>
+            <img
+              src={history}
+              alt="quill pen sitting on top of book"
+              style={{ maxHeight: matchesMD ? 200 : "22em" }}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default About
+export default About;
